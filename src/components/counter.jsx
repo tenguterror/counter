e095013b5f75005eb332e333785a1a1cb74a3264
Duard <vegasduard@gmail.com>
2022-03-18 15:29:20 -0600

    just messing around with differnet state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 export const Counter = () => {
   const [count, setCount] = useState(0);
   const increment = () => {
-    setCount(count + 1)
+    setCount(prevCount => prevCount + 1)
   }
 
   const decrement = () => {
-    setCount(count -1)
+    setCount(prevCount => prevCount - 1)
   }
 
   return (
