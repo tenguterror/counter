0f02ded7243c68511982ba799252b7e3459b99f2
Duard <vegasduard@gmail.com>
2022-03-19 12:18:50 -0600

    Added some comments to how the app works

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+
+  // This will bring the count up one at a time starting at zero
   const increment = () => {
     setCount((prevCount) => prevCount + 1);
   };
 
+  // This will lower the count one at a time
   const decrement = () => {
     setCount((prevCount) => prevCount - 1);
   };
 
+  // This will disable the minus button if at zero to not allow count to go below zero. Either true or false
   const onCountZero = count === 0;
 
+  // This will reset the count if the reset button is clicked
   const resetCount = () => {
     setCount(0);
   };
